37420848d19f641ea87cd868bc4b6940b34bed9b
crazyoptimist <crazyoptimist@mail.com>
2022-11-04 03:59:46 -0500

    feat: implement addNewProduct mutation

--- products/products.resolvers.js
+++ products/products.resolvers.js
@@ -13,4 +13,10 @@ module.exports = {
       return productsModel.getProductById(args.id);
     },
   },
+  Mutation: {
+    addNewProduct: (_, args) => {
+      const { id, description, price } = args;
+      return productsModel.addNewProduct(id, description, price);
+    },
+  },
 };
